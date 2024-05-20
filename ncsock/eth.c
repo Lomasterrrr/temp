809469458ae974215a5980fa71981f5e24d58d9d
lomaster <artemiykastapro@gmail.com>
2024-05-20 19:24:40 +0300

    update

--- ncsock/eth.c
+++ ncsock/eth.c
@@ -16,214 +16,81 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/ioctl.h>
-#include <net/bpf.h>
-#include <net/if.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <sys/sysctl.h>
-#include <net/route.h>
-#include <net/if_dl.h>
-
-#define BPF_DEV "/dev/bpf0"
-#ifndef ETHER_ADDR_LEN
-  #define ETHER_ADDR_LEN 6
-#endif
-
-typedef unsigned char uchar;
-typedef long long vlong;
-
-static uchar *pktbuf = NULL;
-static int pktbufsz = 0;
-
-static int pktn = 0;
-static uchar *pktbp = NULL;
 
 struct eth_handle { int fd; char device[16]; };
-
 int eth_fd(eth_t *e) {
   return e->fd;
 }
 
 eth_t *eth_open(const char *device)
 {
-    char m;
-    int fd = -1;
-    struct bpf_version bv;
-    unsigned bufsize, linktype;
-    char bpf_device[sizeof BPF_DEV];
-    struct ifreq ifr;
+  struct ifreq ifr;
+  char file[32];
+  eth_t *e;
+  int i;
 
-    struct bpf_insn insns[] = {
-        BPF_STMT(BPF_RET | BPF_K, (u_int)-1),  // Пропустить все пакеты
-    };
-
-    struct bpf_program bpf_program = {
-        .bf_len = sizeof(insns) / sizeof(struct bpf_insn),
-        .bf_insns = insns,
-    };
-
-    strncpy(bpf_device, BPF_DEV, sizeof BPF_DEV);
-
-    /* find a bpf device we can use, check /dev/bpf[0-9] */
-    for (m = '0'; m <= '9'; m++) {
-        bpf_device[sizeof(BPF_DEV)-2] = m;
-
-        if ((fd = open(bpf_device, O_RDWR)) > 0)
-            break;
-    }
-
-    if (fd < 0) {
-        perror("open");
-        return NULL;
-    }
-
-    if (ioctl(fd, BIOCVERSION, &bv) < 0) {
-        perror("BIOCVERSION");
-        goto bad;
-    }
-
-    if (bv.bv_major != BPF_MAJOR_VERSION || bv.bv_minor < BPF_MINOR_VERSION) {
-        fprintf(stderr, "kernel bpf filter out of date\n");
-        goto bad;
-    }
-
-    /*
-     * Try finding a good size for the buffer; 65536 may be too
-     * big, so keep cutting it in half until we find a size
-     * that works, or run out of sizes to try.
-     *
-     */
-    for (unsigned v = 65536; v != 0; v >>= 1) {
-        (void) ioctl(fd, BIOCSBLEN, (caddr_t)&v);
-
-        (void)strncpy(ifr.ifr_name, device, sizeof(ifr.ifr_name));
-        if (ioctl(fd, BIOCSETIF, (caddr_t)&ifr) >= 0)
-            break;  /* that size worked; we're done */
-
-        if (errno != ENOBUFS) {
-            fprintf(stderr, "BIOCSETIF: %s: %s\n", device, strerror(errno));
-            goto bad;
-        }
-    }
-    
-    if (pktbufsz == 0) {
-        fprintf(stderr, "BIOCSBLEN: %s: No buffer size worked\n", device);
-        goto bad;
-    }
-
-    /* Allocate memory for the packet buffer */
-    if ((pktbuf = malloc(pktbufsz)) == NULL) {
-        perror("malloc");
-        goto bad;
-    }
-
-    /* Don't wait for buffer to be full or timeout */
-    unsigned v = 1;
-    if (ioctl(fd, BIOCIMMEDIATE, &v) < 0) {
-        perror("BIOCIMMEDIATE");
-        goto bad;
-    }
-
-    /* Only read incoming packets */
-    v = 0;
-    if (ioctl(fd, BIOCSSEESENT, &v) < 0) {
-        perror("BIOCSSEESENT");
-        goto bad;
-    }
-
-    /* Don't complete ethernet hdr */
-    v = 1;
-    if (ioctl(fd, BIOCSHDRCMPLT, &v) < 0) {
-        perror("BIOCSHDRCMPLT");
-        goto bad;
-    }
-
-    /* Get the data link layer type. */
-    if (ioctl(fd, BIOCGDLT, (caddr_t)&v) < 0) {
-        perror("BIOCGDLT");
-        goto bad;
-    }
-    linktype = v;
-
-    /* Get the filter buf size */
-    if (ioctl(fd, BIOCGBLEN, (caddr_t)&v) < 0) {
-        perror("BIOCGBLEN");
-        goto bad;
-    }
-    bufsize = v;
-
-    if (ioctl(fd, BIOCSETF, &bpf_program) < 0) {
-        perror("BIOSETF");
-        goto bad;
-    }
-
-    eth_t *e = malloc(sizeof(eth_t));
-    if (!e) {
-        perror("malloc");
-        goto bad;
-    }
-
-    e->fd = fd;
-    strncpy(e->device, device, sizeof(e->device));
+  e = calloc(1, sizeof(*e));
+  if (!e)
     return e;
-
-bad:
-    if (pktbuf) {
-        free(pktbuf);
-        pktbuf = NULL;
-    }
-    if (fd >= 0) {
-        close(fd);
-    }
-    return NULL;
+  
+  for (i = 0; i < 128; i++) {
+    snprintf(file, sizeof(file), "/dev/bpf%d", i);
+    e->fd = open(file, O_RDWR);
+    if (e->fd != -1 || errno != EBUSY)
+      break;
+  }
+  if (e->fd < 0)
+    return (eth_close(e));
+  
+  memset(&ifr, 0, sizeof(ifr));
+  strlcpy(ifr.ifr_name, device, sizeof(ifr.ifr_name));
+  
+  if (ioctl(e->fd, BIOCSETIF, (char *)&ifr) < 0)
+    return (eth_close(e));
+  i = 1;
+  if (ioctl(e->fd, BIOCPROMISC, &i) == -1)
+    return (eth_close(e));
+  i = 0;
+  if (ioctl(e->fd, BIOCIMMEDIATE, &i) == -1)
+    return (eth_close(e));
+  struct bpf_insn insns[] = {
+    BPF_STMT(BPF_RET | BPF_K, 0),
+  };
+  struct bpf_program fcode = {
+    .bf_len = sizeof(insns) / sizeof(struct bpf_insn),
+    .bf_insns = insns,
+  };
+  if (ioctl(e->fd, BIOCSETF, &fcode) == -1)
+    return (eth_close(e));
+  /*  
+  i = 1;
+  if (ioctl(e->fd, BIOCSHDRCMPLT, &i) < 0)
+    return (eth_close(e));
+  */
+  
+  strlcpy(e->device, device, sizeof(e->device));
+  return (e);
 }
 
 eth_t *eth_close(eth_t *e)
 {
-    if (e != NULL) {
-        if (e->fd >= 0)
-            close(e->fd);
-        free(e);
-    }
-    return NULL;
+  if (e != NULL) {
+    if (e->fd >= 0)
+      close(e->fd);
+    free(e);
+  }
+  return (NULL);
 }
 
 ssize_t eth_send(eth_t *e, const void *buf, size_t len)
 {
-    return write(e->fd, buf, len);
+  return (write(e->fd, buf, len));
 }
 
-ssize_t eth_read(eth_t *e, u8 *buf, ssize_t len) 
+ssize_t eth_read(eth_t *e, u8 *buf, ssize_t len)
 {
-    register struct bpf_hdr *bh;
-    register int pktlen, retlen;
-    
-    if (pktbufsz <= 0) {
-        if ((pktbufsz = read(e->fd, pktbuf, pktbufsz)) < 0) {
-            perror("read");
-            return -1;
-        }
-        pktbp = pktbuf;
-    }
-
-    bh = (struct bpf_hdr *) pktbp;
-    retlen = (int) bh->bh_caplen;
-    /* This memcpy() is currently needed */ 
-    memcpy(buf, (void *)(pktbp + bh->bh_hdrlen), retlen > len ? len : retlen);
-    pktlen = bh->bh_hdrlen + bh->bh_caplen; 
-    
-    pktbp = pktbp + BPF_WORDALIGN(pktlen);
-    pktbufsz -= (int) BPF_WORDALIGN(pktlen);
-
-    return retlen;
+  return read(e->fd, buf, len);
 }
-
 #endif
 #if defined(IS_LINUX)
 #include "include/sys/debianfix.h"
